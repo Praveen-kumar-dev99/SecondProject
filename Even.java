b6d44e3d5a83e44a58b93d8552cce90eebb12c5a
praveen-kumar-dev99 <mpraveenkumar.dev@gmail.com>
2026-04-19 07:55:27 +0800

    program fixed

--- Even.java
+++ Even.java
@@ -3,9 +3,9 @@ public class Even{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        if(n==0){
-            System.out.println("It is a zero number");
-        }else if(n%2==0){
+        if(n%2==0){
+            System.out.println("It is a even number");
+        }else if(n%2!=0){
             System.out.println("It is a odd number");
         }else{
             System.out.println("Invalid input");
