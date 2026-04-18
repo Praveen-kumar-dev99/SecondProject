0206c8f410881998db936cd6b64b2137b7bc14e3
praveen-kumar-dev99 <mpraveenkumar.dev@gmail.com>
2026-04-19 07:46:07 +0800

    program added

---
+++ Even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Even{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==0){
+            System.out.println("It is a zero number");
+        }else if(n%2==0){
+            System.out.println("It is a odd number");
+        }else{
+            System.out.println("Invalid input");
+        }
+        }
+
+    }
